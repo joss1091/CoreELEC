7d4127baad8b2ee1a92519facc9b2b9150408a9f
jose vergara <joss1091@gmail.com>
2019-02-04 20:23:39 +0000

    partch para settings default kodi

--- packages/mediacenter/kodi-theme-Ghost/package.mk
+++ packages/mediacenter/kodi-theme-Ghost/package.mk
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-PKG_NAME="kodi-theme-Bluerobot"
+PKG_NAME="kodi-theme-Ghost"
 PKG_VERSION="9f5927745b1c3b2b2e1a4c8bbc09f4b4282f777c"
 PKG_LICENSE="GPL"
-PKG_SITE="http://159.89.43.103/tvstream/skin.bluerobot"
-PKG_URL="http://159.89.43.103/tvstream/skin.bluerobot/-/archive/master/$PKG_VERSION.tar.gz"
+PKG_SITE="http://159.89.43.103/tvstream/skin.ghost"
+PKG_URL="http://159.89.43.103/tvstream/skin.ghost/-/archive/master/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET=""
 PKG_NEED_UNPACK="$(get_pkg_directory $MEDIACENTER)"
 PKG_LONGDESC="Kodi Mediacenter default theme."
